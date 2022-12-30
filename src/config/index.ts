3687cf0859d2477480ab8389539b228d76a47425
Ray Nirola <ray@nirola.in>
2022-12-30 12:14:33 +0530

    update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,9 @@ dotenv.config()
 
 const config = {
   port: process.env.PORT,
+  app: {
+    key: process.env.APP_KEY
+  },
   openai: {
     password: process.env.OPENAI_PASSWORD,
     email: process.env.OPENAI_EMAIL
