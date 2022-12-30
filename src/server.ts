c77613008dc0e0d00306b723246d235f3a52d952
Ray Nirola <ray@nirola.in>
2022-12-30 12:04:54 +0530

    log req

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,15 @@
 import express from 'express'
+import morgan from 'morgan'
 import config from '@/config'
 import * as routes from '@/routes'
 import ErrorMiddleware from '@/middlewares/ErrorMiddleware'
-import Cron from '@/cron'
 
 const app = express()
-
+app.use(morgan(':method :url :status - :response-time ms'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', routes.home)
 app.use('/chats', routes.chat)
 app.use(ErrorMiddleware)
 
-app.listen(config.port, () => {
-  console.log(`Server is running at ${config.port}`)
-  Cron.start()
-  console.log('Cron started')
-})
+app.listen(config.port, () => console.log(`Server is running at ${config.port}`))
