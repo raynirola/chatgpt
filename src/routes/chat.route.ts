2d3b6c86a39b991c2c22684ba2064b107aca38b8
Ray Nirola <ray@nirola.in>
2022-12-31 20:05:08 +0530

    validate `POST` `/chats` route

--- src/routes/chat.route.ts
+++ src/routes/chat.route.ts
@@ -1,8 +1,24 @@
+import { z } from 'zod'
 import { Router } from 'express'
 import { ChatController } from '@/controllers'
+import ValidateRequest from '@/middlewares/Validate'
+
+const createChatSchema = z.object({
+  message: z
+    .string({
+      required_error: 'Message is a required field',
+      invalid_type_error: 'Please provide a valid message'
+    })
+    .min(20, {
+      message: 'Message must be at least 20 characters long'
+    })
+    .max(2048, {
+      message: 'Message must be at most 2048 characters long'
+    })
+})
 
 const chat = Router() as Router
 
-chat.post('/', ChatController.create)
+chat.post('/', ValidateRequest(createChatSchema), ChatController.create)
 
 export default chat
